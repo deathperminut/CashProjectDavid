91ffd58f1d3e7f5ddc5872bd9612192712f917f8
Marco Zuñiga <marcohurtado358@gmail.com>
2022-09-05 18:13:36 -0500

    Codigo limpio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './components/nav/nav.component';
+import { TableComponent } from './components/table/table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    TableComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule, FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/table/coin.model.ts
@@ -0,0 +1,7 @@
+export interface Coin{
+  coin_name:string;
+  arbitrage_percentage:string;
+  state:boolean;
+  quantity:number;
+  action:string
+}
