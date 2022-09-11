036baa775e080904ad37c1441e05ba9e8650d4ce
Marco Zuñiga <marcohurtado358@gmail.com>
2022-09-10 20:34:36 -0500

    implementación del boton send

--- src/app/interfaces/coin.model.ts
+++ src/app/interfaces/coin.model.ts
@@ -1,7 +1,12 @@
 export interface Coin{
+  userId: string;
+  btc:string;
+  firstQuantity:number;
+  lastPrice:number;
+  exchange:boolean,
+  price:number;
   coin_name:string;
   arbitrage_percentage:string;
   state:boolean;
   quantity:number;
-  action:string
 }
