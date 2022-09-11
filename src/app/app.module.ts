1a222e369152c8292fd0cd0ee099bb02f5fc1d9a
deathperminut <mendezsebas8@gmail.com>
2022-09-11 13:44:58 -0500

    SE HACE LA IMPLEMENTACIÓN DE RUTAS PARA EL FORM, Y SE CREA EL COMPONENTE DEL FORMULARIO YA COMPLETO CON RESPONSIVE...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NavComponent } from './components/nav/nav.component';
 import { TableComponent } from './components/table/table.component';
+import { FormComponent } from './components/form/form.component';
+import { RouterModule, Routes } from '@angular/router'; // IMPORTAMOS PARA GENERAL LAS RUTAS
+
+// CREAMOS MI CONSTANTE DE RUTAS
+// AL INICIO NO RECONOCE LA CLASE ROUTES PORQUE TOCA IMPORTARLA
+// DEL PAQUETE DE ANGULAR.
+
+const appRoutes:Routes=[
+  {path:'',component:TableComponent},
+  {path:'Form',component:FormComponent}
+
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     TableComponent,
+    FormComponent,
   ],
   imports: [
-    BrowserModule, FormsModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes) // LE ENVIAMOS LA CONSTANTE CON MIS RUTAS
   ],
   providers: [],
   bootstrap: [AppComponent]
